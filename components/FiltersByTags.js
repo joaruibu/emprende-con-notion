986d9e200a2833f11e53ffc2d6a96a29fab138c6
Joaquín Ruiz Bueno <65560673+joaruibu@users.noreply.github.com>
2024-03-16 22:51:19 +0000

    shadow mobile version tag bar | update sitemap

--- components/FiltersByTags.js
+++ components/FiltersByTags.js
@@ -25,18 +25,21 @@ const FiltersByTags = ({ tags, setTags, setItems, type }) => {
     }
 
     return (
-        <div className='border-2 border-stone-950 bg-white rounded-md p-2 mt-2 min-w-full flex-none overflow-x-scroll'>
-            {domLoaded &&
-                <div className='flex gap-2'>
-                    {
-                        tags.map((tag, index) => {
-                            return <h4 key={index} onClick={() => onClickTag(index)} className={`inline-flex items-center rounded px-4 py-1 text-xs font-medium border-2 border-stone-200 hover:border-2 hover:border-stone-950 cursor-pointer transition-all ${tag.selected && ' border-2 border-stone-800 '}`}>
-                                {tag.name}
-                            </h4>
-                        })
-                    }
-                </div>
-            }
+        < div className="relative">
+            < div className='sm:hidden h-11 w-6 top-[2px] right-[2px]  bg-gradient-to-r absolute from-gray-100/50 to-gray-300 z-10' ></ div>
+            <div className='border-2 relative border-stone-950 bg-white rounded-md p-2 mt-2 min-w-full flex-none overflow-x-scroll'>
+                {domLoaded &&
+                    <div className='flex gap-2'>
+                        {
+                            tags.map((tag, index) => {
+                                return <h4 key={index} onClick={() => onClickTag(index)} className={`inline-flex items-center rounded px-4 py-1 text-xs font-medium border-2 border-stone-200 hover:border-2 hover:border-stone-950 cursor-pointer transition-all ${tag.selected && ' border-2 border-stone-800 '}`}>
+                                    {tag.name}
+                                </h4>
+                            })
+                        }
+                    </div>
+                }
+            </div>
         </div>
     )
 }
