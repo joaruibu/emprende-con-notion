0f05bbc896536f8734ede3c5a6be4b113a9f9805
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2025-02-04 16:43:26 +0000

    chore: fixed texts

--- pages/formacion/grupo-privado-plantilla-zettelkasten.js
+++ pages/formacion/grupo-privado-plantilla-zettelkasten.js
@@ -5,7 +5,7 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
   const STRIPE_BUTTON_CONFIG = {
     title: "Plantilla Zettelkasten para grupo privado.",
     buttonText: " Acceder ahora",
-    price: "60€",
+    price: "60 €",
     isDisabled: false,
     urlStripe:
       "https://lauradearquer.quadernoapp.com/checkout/session/6c1300c58f98ba86fdbf82ac6869f4fdc730f739/init",
@@ -37,7 +37,12 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
             <br></br>
             <br></br>
           </div>
-          <p>Si has llegado a esta página ya sabes lo que es esto.</p>
+          <p>
+            Si has llegado a esta página ya sabes de lo que va esto. La
+            plantilla estará disponible para{" "}
+            <b>un grupo de máximo 10 personas</b> y la seguiré vendiendo durante
+            este mes o hasta que se agoten plazas.
+          </p>
           <p>
             <b>Accedes al grupo privado aquí: </b>{" "}
           </p>
@@ -58,9 +63,9 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
             <br></br>
             <p>
               Te llegará automáticamente un email de confirmación de compra con
-              un enlace y contraseña para acceder al contenido del Workshop: la
-              plantilla, el enlace para el próximo directo del viernes, y
-              cualquier otro recurso que te quiera dar.
+              un enlace y contraseña para acceder al contenido del grupo
+              privado: la plantilla, el enlace para el próximo directo del
+              viernes, y cualquier otro recurso que te quiera dar.
             </p>
           </div>
           <div className="pb-12">
@@ -92,13 +97,14 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
                   ¿se te ha ido la pinza con el precio tan bajo? ¿Eso no es lo
                   que se hace en una consultoría?{" "}
                 </i>{" "}
-                En parte sí. Pero va a ser un toma y daca. Tú me vas a preguntar
-                y yo voy a poder ver en directo cómo usas la plantilla y ver tus
-                necesidades, para luego venderla mas cara. Todos ganamos.{" "}
+                En parte sí. Pero el grupo va a ser un toma y daca: tú me vas a
+                preguntar y yo voy a poder ver en directo cómo usas la plantilla
+                y tus necesidades para luego venderla a mayor precio. Todos
+                ganamos.{" "}
               </li>
               <li>
                 <b>
-                  Posteriormente tendrás acceso a la plantilla definitiva que se
+                  Posteriormente tendrás acceso a la plantilla 2.0 que se
                   venderá a mayor precio.{" "}
                 </b>
               </li>
@@ -117,9 +123,9 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
               compras el día 3, el mes menos dos días. Y así.
             </p>
             <p>
-              Además tendrás soporte en directo donde podrás compartir pantalla
-              si quieres y enseñarme directamente lo que necesitas hacer en tu
-              Notion.{" "}
+              Además podrás participar en las sesiones grupales semanales, donde
+              podrás compartir pantalla si quieres y enseñarme directamente lo
+              que necesitas adaptar en tu Notion.
             </p>
           </div>
           <div className="pb-12">
@@ -128,7 +134,7 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
             </h2>
             <br></br>
             <p>
-              Para participar en este Workshop te recomiendo tener un nivel
+              Para participar en este grupo privado te recomiendo tener un nivel
               mínimo. No vale que no hayas abierto la aplicación en tu vida,
               tienes que saber por lo menos lo que es un bloque, una base de
               datos, cosas así. En el directo te voy a explicar a usar la
@@ -146,7 +152,15 @@ const GrupoPrivadoPlantillaZettelkasten = () => {
             <br></br>
             <p>
               El precio es de {STRIPE_BUTTON_CONFIG.price} y nunca bajará. Es
-              más, en la próximas semanas subirá.
+              más, dado todo el soporte que hay, subirá las próximas semanas.
+            </p>
+            <p>
+              El objetivo de este grupo privado precisamente es entender mejor
+              tus necesidades para luego sacar una plantilla 2.0, más cara, a
+              partir del mes que viene. Si tú quieres comprarla el mes que viene
+              por 100 €, está bien. Puedes esperar. En caso de que quieras
+              comprar ahora por {STRIPE_BUTTON_CONFIG.price}, te apuntas al
+              grupo aquí:
             </p>
             <p>
               Accedes al grupo reducido, con precio reducido (hasta el día 28 a
