04a714e235809889178111405d53616bdd83ab55
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2025-03-25 09:42:44 +0000

    chore: updare next config standalone

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
