3eb435c340ed499fc96d560ec4dbd6cde07e787d
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2025-05-16 11:20:08 +0100

    chore: wip second brain

--- pages/formacion/segundo-cerebro-en-notion-estas-dentro.js
+++ pages/formacion/segundo-cerebro-en-notion-estas-dentro.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import FormAccesContent from "../../components/FormAccesoContentido";
 import CountdownWidget from "../../components/CountdownWidget";
+import Desplegable from "../../components/Desplegable";
 
 const GrupoPrivadoPlantillaZettelkastenEstasDentro = () => {
   const [password, setPassword] = useState("");
@@ -31,26 +32,149 @@ const GrupoPrivadoPlantillaZettelkastenEstasDentro = () => {
           <div className=" sm:overflow-hidden sm:rounded-xl"></div>
           <section className="pt-10 ">
             <h1 className="mb-4 font-alternate text-4xl leading-[52px] text-center">
-              Curso: Tu segundo cerebro en Notion.
+              Curso Segundo Cerebro.<br></br>
+              Trabaja a tu favor.
             </h1>
             <br></br>
             <br></br>
             <br></br>
             <div className=" mb-24">
-              <h3 className="mb-12">
-                Próximamente encontrarás aquí toda la información del
-                curso.{" "}
-              </h3>
-              <p>
-                Como has comprado en preventa esta página está por ahora
-                desactivada. No te preocupes, te avisaré por email cuando el
-                contenido esté visible.{" "}
+              <i className="font-bold text-xs">
+                ***Por favor, lee hasta el final, hay información importante y
+                no me gustaría que te perdieras nada.
+              </i>
+              <h2 className="mt-24 font-alternate text-3xl leading-[52px] mb-12">
+                - Para acceder al próximo directo:
+              </h2>
+              <a href={""} target="_blank">
+                <button
+                  className=" opacity-20 rounded-md w-full border border-stone-950 bg-yellow-300 p-4 text-xl font-sub text-zinc-900 font-semibold  "
+                  type="submit"
+                  disabled
+                >
+                  Clica para acceder al directo
+                </button>
+              </a>
+
+              <h2 className="mt-24 font-alternate text-3xl leading-[52px]">
+                - Instrucciones de uso.
+              </h2>
+              <ol className="mb-12">
+                <li>
+                  Sigue el curso <u>por orden. </u> (Esto es importante.) .
+                </li>
+                <li>
+                  No te saltes ninguna lección. Te voy a dar recursos valiosos.
+                </li>
+                <li>
+                  Haz las prácticas. No las saltes por pereza ni porque pienses
+                  que son cosas que ya sabes. Todas tienen un propósito.
+                </li>
+                <li>Comprométete a acabarlo.</li>
+              </ol>
+
+              <p className="border-l-2 pl-4 flex flex-col gap-4 mb-12">
+                <q>
+                  Dicho esto, gracias por volver a confiar en mí —si ya has
+                  hecho otro curso conmigo— o por confiar en mi trabajo por
+                  primera vez. Escribo esto con mucho agradecimiento. Verás. Amo
+                  enseñar lo que estudio para poder aprenderlo. Gracias por
+                  ayudarme a materializar uno de mis sueños, que es enseñar
+                  desarrollo personal.
+                </q>
+                <q>
+                  Porque, no te equivoques, la productividad es una pata del
+                  desarrollo personal, y te voy a enseñar a trabajarla con la
+                  importancia y dedicación que merece en tu vida.{" "}
+                </q>
+                <q>
+                  En este curso de Segundo Cerebro voy compartir contigo
+                  herramientas que me parecen imprescindibles.
+                </q>
+                <q>
+                  Quizás algunas, ya las conozcas. Si es el caso, te recomiendo
+                  que las vuelvas a analizar porque, dependiendo del momento
+                  vital en el que te encuentres, tu interpretación puede ser
+                  totalmente diferente y traer nueva información que te sea útil
+                  para el trabajo.
+                </q>
+                <q>Gracias por dejarme acompañarte. :)</q>
+                <q>Laura.</q>
               </p>
-              <p>
-                Por ahora, apunta en tu agenda nuestra primera sesión el domingo
-                18 de mayo a las 17.00 h (Valencia).{" "}
-              </p>
-              <CountdownWidget />
+
+              <Desplegable title="Módulo 0.  Primera parada.">
+                <br></br>
+                <a href={"Empieza aquí. "} target="_blank">
+                  <button
+                    className=" mb-12 rounded-md w-full border border-stone-950 bg-yellow-300 text-xl text-stone-950 p-4 font-alternate uppercase font-semibold duration-150 hover:shadow-dark "
+                    type="submit"
+                  >
+                    Empieza aquí.
+                  </button>
+                </a>
+
+                <p>
+                  * Si aún no sabes duplicar plantillas, te lo explico en este
+                  <a
+                    href="https://youtu.be/Y6rBNkV4rnY?si=0oHMxHKf4NhGAIkp"
+                    target="_blank"
+                  >
+                    {" "}
+                    Short de menos de 1 minuto.
+                  </a>
+                </p>
+              </Desplegable>
+
+              <Desplegable title="Módulo 1. Qué me pasa doctor.">
+                <CountdownWidget></CountdownWidget>
+              </Desplegable>
+
+              <Desplegable title="Módulo 2. Medisina.">
+                <CountdownWidget></CountdownWidget>
+              </Desplegable>
+              <Desplegable title="Módulo 3. Cual zarapito trinador.">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+              <Desplegable title="Módulo 3. Cual zarapito trinador.">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+              <Desplegable title="Módulo 3. Cual zarapito trinador.">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+              <Desplegable title="Módulo 4. Onirograma y otros psicodélicos.">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+              <Desplegable title="Módulo 5. Coge tu agenda por los cuernos.">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+              <Desplegable title="Módulo 6. Y tú, ¿qué consumes?">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+              <Desplegable title="Bonus. Curso de Notion Exprés.">
+                <p className="m-6">
+                  Próximamente… Mira el apartado de planificación para agenciar
+                  ya.
+                </p>
+              </Desplegable>
+
               <p className="pt-8">
                 P.D.: Cualquier duda, sugerencia, problema, saludos, me escribes
                 a laura@emprendeconnotion.com.{" "}
